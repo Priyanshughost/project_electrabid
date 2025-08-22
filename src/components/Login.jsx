a9b59bfb5e1627508f9ad945b16e9383502c11d9
Priyanshu Ghosh <111736328+Priyanshughost@users.noreply.github.com>
2025-08-22 11:36:24 +0530

    Add files via upload

---
+++ src/components/Login.jsx
@@ -0,0 +1,66 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+function Login() {
+  return (
+    <div className='relative log-con'>
+        <div className="float absolute w-[40%] bg-white">
+            <h2 className=' bg-[#484848] text-white text-center'>Login in to Government e Marketplace | GeM</h2>
+            <div className="log-sec flex flex-col gap-3 text-[0.8rem]">
+                <div className="log-top flex flex-col gap-4">
+                    <h3>GeM User Id </h3>
+                    <input type="text" className='outline-none' />
+                </div>
+                <div className="log-mid flex flex-col gap-4">
+                    Type the characters in the box below. 
+                    <div className="mid-box flex justify-between items-center flex-wrap gap-4">
+                        <input type="text" className='outline-none rounded-[0.5rem] bg-[#cbcbcb]'/>
+                        <h4>we1ty3</h4>
+                        <img src="https://sso.gem.gov.in/ARXSSO/images/refresh.png" alt="" className='w-[2rem]'/>
+                    </div>
+                </div>
+                <p>Password shall be entered on next screen post successful GeM login ID and Captcha validation</p>
+                <button className='w-max rounded-2xl bg-[#f57a10] text-white'>Submit</button>
+            </div>
+        </div>
+        <div className="not-reg flex flex-col gap-2 text-[#484848]">
+            <h2>Not Registered with GeM ? Sign up to experience the Marketplace</h2>
+            <div className="btns flex gap-8">
+                <Link to="/registration/signUp/buyer" className='flex items-center gap-[0.4rem] text-[#093385]'>
+                    <img src="https://sso.gem.gov.in/ARXSSO/images/new_theme_images/buyer-organisation.svg" alt="" />
+                    <h2>Register as Buyer</h2>
+                </Link>
+                <Link to="/registration/signUp/seller" className='flex items-center gap-[0.4rem] text-[#093385]'>
+                    <img src="https://sso.gem.gov.in/ARXSSO/images/new_theme_images/sellers-serivce-providers.svg" alt="" />
+                    <h2>Register as Seller</h2>
+                </Link>
+            </div>
+        </div>
+        <div className="help bg-[url(https://sso.gem.gov.in/ARXSSO/images/login-back_v1-min.png)] text-white flex flex-col gap-4 bg-no-repeat bg-cover">
+            <h1 className='text-[1.7rem]'>Need help with your Login?</h1>
+            <div className="raisetckt">
+                <div className="icon-head flex gap-[0.5rem]">
+                    <img src="https://sso.gem.gov.in/ARXSSO/images/new_theme_images/ticket.svg" alt="" />
+                    <h3 className='font-semibold'>Raise a ticket</h3>
+                </div>
+                <p>
+                    Agents and experts available on a single platform ready to help you
+                </p>
+            </div>
+            <div className="contact">
+                <div className="icon-head flex">
+                    <img src="https://sso.gem.gov.in/ARXSSO/images/new_theme_images/phone-new.svg" alt="" />
+                    <h3 className='font-semibold'>Contact Us</h3>
+                </div>
+                <div className="details">
+                    <p>Email: helpdesk-gem[at]gov[dot]in</p>
+                    <p>Call: 1-1800-419-3436 / 1-1800-102-3436</p>
+                    <p>(9:00 a.m. - 10:00 p.m. Mon to Sat)</p>
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Login
---
+++ src/components/SellerSignUp.jsx
@@ -0,0 +1,63 @@
+import React from 'react'
+
+function SellerSignUp() {
+    return (
+        <div>
+            <div className="signup-top">
+                <h1 className='text-xl text-[#7e7e7e]'>SignUp &gt;</h1>
+                <div className="reg flex justify-between">
+                    <h5 className='text-2xl font-semibold'>Seller/Service Provider Registration</h5>
+                    <a href="#" className="flex text-sm items-center gap-1 text-red-600">
+                        <h5>Need help with Registration?</h5>
+                        <img src="https://cdn.hugeicons.com/icons/youtube-solid-standard.svg" alt="" style={{ width: 1.3 + 'rem' }} />
+                    </a>
+                </div>
+                <div className="process flex justify-around bg-[#f3f3f3] rounded-2xl">
+                    <div className="first flex gap-2 text-blue-600 font-semibold text-xl">
+                        <h3 className=''>1.</h3>
+                        <p>Pre-requisites</p>
+                    </div>
+                    <div className="second flex gap-2 text-xl text-gray-700">
+                        <h3>2.</h3>
+                        <p>Terms & Conditions</p>
+                    </div>
+                    <div className="third flex gap-2 text-xl text-gray-700">
+                        <h3>3.</h3>
+                        <p>Registration</p>
+                    </div>
+                </div>
+            </div>
+            <div className="signup-bot flex flex-col">
+                <div className="redirect text-[0.9rem] text-right">
+                    Already Registered with Ministry of Textiles As Weaver or Artisan?<a href="#" className='text-blue-600 font-semibold'>Click Here</a>
+                </div>
+                <div className="pre-req flex flex-col gap-4 rounded-[0.3rem]">
+                    <div className="pre-req-top flex flex-col gap-[0.2rem]">
+                        <h1 className='text-[1.8rem] font-semibold text-[#2e2e2e]'>Pre-requisites</h1>
+                        <p className='text-[#4e4e4e]'>Registration on GeM should be done by an authorized person (Director of the organisation or a Key Person/Proprietor).</p>
+                    </div>
+                    <div className="pre-req-form flex flex-col gap-[0.2rem]">
+                        <h2 className='text-[1.1rem]'>Business / Organisation Type</h2>
+                        <div className="io flex justify-between">
+                            <select className="outline-none w-1/2 rounded-xl">
+                                <option value="">Select type of User</option>
+                                <option value="hod">Primary User (HOD)</option>
+                                <option value="va">Verifying Authority</option>
+                            </select>
+
+                            <button className='text-[1.2rem] font-semibold text-[#8f8f8f] bg-[#d5d5d5] rounded-xl'>Proceed</button>
+                        </div>
+                    </div>
+                    <a href="#" className="pre-req-bot text-blue-700 font-semibold">
+                        VIEW PRE-REQUISITES DOCUMENT
+                    </a>
+                </div>
+                <div className="login text-[0.9rem]">
+                    Already registered with GeM?<a href="#" className='text-blue-600 font-semibold'>Click Here To Login</a>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default SellerSignUp
